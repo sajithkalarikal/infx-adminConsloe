78fea6da3b516d0499d953cee2e6683020d21fbd
srishti <srishti@locus.sh>
2018-07-24 01:51:39 +0530

    added tabledescription title

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,35 @@
 var isReadOnly = true;
 var new_object = [];
-function saveToCSVTable() {
-// on confirm Save
-     var url = '/updateXML';
-     var body = {
-        data: new_object
-     };
-     fetch(url, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      })
-      .then(function(res) {
-        console.log(res)
-		var d = document.getElementById('messagebox');
-		d.textContent="Updates Will Reflect In 24 Hours";
-		setTimeout(function(){ d.textContent=""; }, 4000);
-		//final popup message.
-		//alert("will reflect in 24hrs")
-		//var popup = document.getElementById('popup');
-		      }).catch(function(err) {
-        console.log(err)
-      })
+var tableDescription = {
+  'Report Threshold': 'Description One',
+  'Report Threshold 2': 'Description two'
 }
 
+function saveToCSVTable() {
+  // on confirm Save
+  var url = '/updateXML';
+  var body = {
+    data: new_object
+  };
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then(function(res) {
+    console.log(res)
+
+    var d = document.getElementById('messagebox');
+    d.textContent="Updates Will Reflect In 24 Hours";
+    // message should disappear after 5 seconds
+    setTimeout(function(){ d.textContent=""; }, 4000);	
+  }).catch(function(err) {
+    console.log(err)
+  })
+}
 
 function  toggleInput(event) {
   var value = event.target.id;
@@ -36,7 +39,7 @@ function  toggleInput(event) {
     inputs[item].disabled = isReadOnly;    
   }
   if (isReadOnly) {
-	// open popup
+	  // open popup
 		var popup = document.getElementById('popup');
 		console.log(popup)
 		popup.style.display = 'block';
@@ -49,30 +52,47 @@ function renderDataToHtml(jsonObject, headerDiv, body) {
   jsonObject.forEach(function(data, i) {
     var container = document.createElement('div');
     container.className = 'col-md-12 data-container';
+
     var div = document.createElement('div');
     div.className = 'col-md-3 table-data';
+
     var input = document.createElement('input');
 
     var keys = Object.keys(data);
+
+    // adding data to table header
     keys.forEach(function(key) {
       if (header.indexOf(key) === 1) { header.push(key) }       
     })
+
+    header.forEach(function(title) {
+      var div = document.createElement('div');
+      div.textContent = title;
+      div.className = 'col-md-3 table-header';
+      headerDiv.appendChild(div);
+    })
+
+    // adding rest of the rows 
     keys.forEach(function(key, itr) {
-      if ( itr === 0 ) {
-        div.textContent = data[key];
-        container.appendChild(div);
+      if ( itr === 0 ) { // first column data of table
+        var content = data[key];
+        div.textContent = content;
+        div.title = tableDescription[content]; // show description of the Threshold data
+        container.appendChild(div); 
       } else {
         input.value = data[key];
-        input.disabled = isReadOnly;
+        input.disabled = isReadOnly; // input is disabled by default
         input.id = i + '_' + key;
         input.className='col-md-3 input-field table-data';
         container.appendChild(input);
       }
+
       body.appendChild(container);
-      
     })
   })
+
   var inputs = document.getElementsByClassName('input-field');
+  // when input is not disabled - call function when editing the input column
   for(var x = 0; x < inputs.length; x++) {
      inputs[x].onchange = function(e) {
         var value = e.target.value;
@@ -83,13 +103,6 @@ function renderDataToHtml(jsonObject, headerDiv, body) {
         console.log(new_object);
      };
   }
-
-   header.forEach(function(title) {
-    var div = document.createElement('div');
-      div.textContent = title;
-      div.className = 'col-md-3 table-header';
-      headerDiv.appendChild(div);
-    })
 
 }
 
@@ -127,7 +140,7 @@ function handleFileSelect() {
   }).catch(function(err) {
     console.log(err)
   })
-  };
+};
   
   
   
