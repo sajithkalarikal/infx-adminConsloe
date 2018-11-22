0b66ab62853acffba0dd0aa2bd9cc641f596962b
sajithkalarikal <sajithkalarikal.92@gmail.com>
2018-11-22 18:27:04 +0530

    added ie css

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./app.js"
+	  error_file : "./err.log",
+    out_file : "./out.log"
+  }]
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,36 @@
 var isReadOnly = true;
 var new_object = [];
-var tableDescription = {
-  'Report Threshold': 'Description One',
-  'Report Threshold 2': 'Description two'
-}
+var json_object = [];
+// var tableDescription = {
+  // 'RPT1081 CNA   Threshold Alerts': 'Description One',
+  // 'RPT1054 Sirius Double Sold': 'Description two',
+  // 'RPT1044 Scems Double Sold Validation': 'Description 3',
+  // 'RPT1084 CNA   Threshold Alerts': 'Description 4'
+// }
+
+
+
 
 function saveToCSVTable() {
   // on confirm Save
   var url = '/updateXML';
-  var body = {
-    data: new_object
-  };
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
-  })
-  .then(function(res) {
+
+  $.post(url, {data: JSON.stringify(new_object)}, function(res) {
     console.log(res)
 
-    var d = document.getElementById('messagebox');
-    d.textContent="Updates Will Reflect In 24 Hours";
+	
+	//var d = document.getElementById('messagebox');
+//    d.textContent="Note: Change's Will Reflect In The Dashboard After The Control Trigger's";
+	
     // message should disappear after 5 seconds
-    setTimeout(function(){ d.textContent=""; }, 4000);	
-  }).catch(function(err) {
-    console.log(err)
-  })
+   // setTimeout(function(){ d.textContent=""; },300000);
+	
+     })
 }
+
+
+
+
 
 function  toggleInput(event) {
   var value = event.target.id;
@@ -47,53 +48,70 @@ function  toggleInput(event) {
   }
 }
 
-function renderDataToHtml(jsonObject, headerDiv, body) {
-  var header = [];
-  jsonObject.forEach(function(data, i) {
-    var container = document.createElement('div');
-    container.className = 'col-md-12 data-container';
+function renderDataToHtml(data, i, headerDiv, body) {
+	var header = [];
+	var keys = Object.keys(data);
+	
+	// adding data to table header
+	keys.forEach(function(key) {
+		if (header.indexOf(key) === -1 ) {
+			header.push(key) 
+		}       
+	})
+	
+	
+	header.forEach(function(title) {
+		var div = document.createElement('div');
+		div.textContent = title;
+		div.className = ' table-header';
+		
+		headerDiv.appendChild(div);
+	})
+	
+	for(var a = 1; a <= 4; a++) {	
+		var x= headerDiv.children.length
+		headerDiv.children[x-a].className = 'backgroundcolor' + a + ' table-header' 
+	}
 
-    var div = document.createElement('div');
-    div.className = 'col-md-3 table-data';
 
-    var input = document.createElement('input');
-
-    var keys = Object.keys(data);
-
-    // adding data to table header
-    keys.forEach(function(key) {
-      if (header.indexOf(key) === 1) { header.push(key) }       
-    })
-
-    header.forEach(function(title) {
-      var div = document.createElement('div');
-      div.textContent = title;
-      div.className = 'col-md-3 table-header';
-      headerDiv.appendChild(div);
-    })
-
+	var container = document.createElement('div');
+	container.className = 'body-container ';
     // adding rest of the rows 
     keys.forEach(function(key, itr) {
-      if ( itr === 0 ) { // first column data of table
-        var content = data[key];
-        div.textContent = content;
-        div.title = tableDescription[content]; // show description of the Threshold data
-        container.appendChild(div); 
-      } else {
-        input.value = data[key];
-        input.disabled = isReadOnly; // input is disabled by default
-        input.id = i + '_' + key;
-        input.className='col-md-3 input-field table-data';
-        container.appendChild(input);
-      }
-
-      body.appendChild(container);
+		if(itr === keys.length - 4 ){
+			var input = document.createElement('input');
+			input.value = data[key];
+			input.disabled = isReadOnly; // input is disabled by default
+			input.id = i + '_' + key ;
+			input.className='input-field table-data';
+			container.appendChild(input);
+		} else {      
+			var content = data[key].trim();
+			var div = document.createElement('div');
+			div.className = 'table-data ';
+			div.textContent = content;
+			//div.title = tableDescription[content]; // show description of the Threshold data
+			container.appendChild(div); 
+        }
     })
-  })
+	body.appendChild(container);
+	// to aligne all the table cell
+	var maxHeight = 0;
+	
+	$('.table-data').each(function (index, node) {
+		var header = $('.table-header')[index];
+		maxHeight = header.offsetHeight;
+		maxHeight = Math.max(maxHeight, node.offsetHeight);
+		header.style.height = maxHeight + "px";
+		node.height = maxHeight + "px";
+		
+	});
 
-  var inputs = document.getElementsByClassName('input-field');
-  // when input is not disabled - call function when editing the input column
-  for(var x = 0; x < inputs.length; x++) {
+
+	
+	var inputs = document.getElementsByClassName('input-field');
+	// when input is not disabled - call function when editing the input column
+	for(var x = 0; x < inputs.length; x++) {
      inputs[x].onchange = function(e) {
         var value = e.target.value;
         var id = e.target.id;
@@ -103,17 +121,32 @@ function renderDataToHtml(jsonObject, headerDiv, body) {
         console.log(new_object);
      };
   }
-
+	
 }
-
+// get file from file
 function handleFileSelect() {
   var url = '/getDataFromFile'
-  fetch(url, {cache: 'no-store'}).then(async function(res) {
-    console.log(res)
-    var response = res.json();
-			    var json_object = await response;
-		console.log(json_object);
+  $.get(url, {cache: 'no-store'}).then( function(res) {
+	json_object = res;
+	console.log(json_object);
     new_object = json_object.slice(0);
+	
+	var select = document.getElementById("sel1"); 
+		// var arryList = document.getelementbyclassName('table-data')
+		
+	var options= [] 
+	new_object.forEach(function(arrayitem){
+		options.push(arrayitem['Report']);	
+	}) 
+
+	for(var i = 0; i < options.length; i++) {
+		var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = i;	
+		 select.appendChild(el)
+	};
+	
     var actionList = ['Edit', 'Save'];
     var action = document.getElementById('actions');
 
@@ -133,17 +166,32 @@ function handleFileSelect() {
     header = document.getElementById('headercontainer');
     body = document.getElementById('bodycontainer');
 
-    renderDataToHtml(json_object, header, body);
+    renderDataToHtml(json_object[0], 0, header, body);
     document.getElementById('Edit').addEventListener('click', toggleInput, false);
     document.getElementById('Save').addEventListener('click', toggleInput, false);
 
+	
   }).catch(function(err) {
     console.log(err)
   })
 };
-  
-  
-  
+  //drop down to slect one report at a time
+function selectreport(value){
+	var header = document.getElementById('headercontainer');
+    var body = document.getElementById('bodycontainer');
+
+	while (body.hasChildNodes()) {
+		body.removeChild(body.lastChild);
+	}
+	while (header.hasChildNodes()) {
+		header.removeChild(header.lastChild);
+	}
+	var rep = json_object[value];
+	renderDataToHtml(rep, value, header, body);
+	
+}
+
+	
   
   
   
